facb692df92b689d33d95f3e1ef501572db15e4f
Abdullah TATLICAK <tatlicak@gmail.com>
2023-11-20 19:17:33 +0300

    Nodemailer - Api Mail Gönderme

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -3,6 +3,9 @@ const bcrypt = require("bcrypt");
 const APIError = require("../utils/errors");
 const Response = require("../utils/response");
 const { createToken } = require("../middlewares/auth");
+const crypto = require("crypto");
+const sendEmail = require("../utils/sendMail");
+const moment = require("moment/moment");
 
 const login = async (req,res) => {
 
@@ -68,10 +71,41 @@ const me = async (req, res) => {
     return new Response(req.user).success(res);
 }
 
+const forgetPassword = async (req,res) => {
+    const { email } = req.body;
 
+    const userInfo = await user.findOne({email}).select(" name lastname email ");
+
+    if (!userInfo) return new APIError("Invalid User", 400);
+
+    console.log(userInfo);
+
+    const resetCode = crypto.randomBytes(3).toString("hex");
+
+    await sendEmail({
+        from:"tatlicak@ataytekbilisim.com",
+        to: userInfo.email,
+        subject: "Şifre Sıfırlama",
+        text: `Şifre Sıfırlama Kodunuz ${resetCode}`
+
+    })
+
+    await user.updateOne(
+        {email},
+        {
+            reset: {
+                code: resetCode,
+                time: moment(new Date()).add(15, "minute").format("YYYY-MM-DD HH:mm:ss")
+            }
+        }
+    )
+
+    return new Response(true, "Please Check Your Mailbox").success(res)
+}
 
 module.exports = {
     login,
     register,
-    me
+    me,
+    forgetPassword
 }
